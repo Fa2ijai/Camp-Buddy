e5bf6540dd1a3c0d743ac442fe8b60818f52090b
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2024-03-31 17:15:43 +0700

    fix: finish auth

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-// app.js
 const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+//Route files
+const auth = require("./routes/auth");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to data base
+connectDB();
+
 const app = express();
-const port = 5000; // Or any other port number you prefer
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//Body parser
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+//Cookie parser
+app.use(cookieParser());
+
+//Mount routers
+app.use("/api/v1/auth", auth);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT
+  )
+);
+
+//Handle unhandle promise rejections
+process.on("inhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
 });
