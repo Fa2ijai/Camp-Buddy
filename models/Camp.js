3f753301aa8bcaad6e4b5212175a824661c9e6f6
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2024-03-31 16:40:33 +0700

    add schemas

---
+++ models/Camp.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const CampSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Name can not be more than 50 characters"],
+    },
+    address: {
+      type: String,
+      required: [true, "Plese add an address"],
+    },
+    district: {
+      type: String,
+      require: [true, "Please add district"],
+    },
+    province: {
+      type: String,
+      required: [true, "Please add a province"],
+    },
+    postalcode: {
+      type: String,
+      required: [true, "Please add a postalcode"],
+      maxlength: [5, "Postal Code can not be more than 5 digits"],
+    },
+    tel: {
+      type: String,
+      required: [true, "Please add a telephone number"],
+      match: [
+        /^0\d{9}$/,
+        "Please enter a valid telephone number starting with 0 and consisting of 10 digits",
+      ],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+//Cascade delete appiontments when a hospital is deleted
+HospitalSchema.pre(
+  `deleteOne`,
+  { document: true, query: false },
+  async function (next) {
+    console.log(`Appointments being removed form hospital ${this._id}`);
+    await this.model("Appointment").deleteMany({ hospital: this._id });
+    next();
+  }
+);
+
+//Reverse populate with virtuals
+HospitalSchema.virtual(`bookings`, {
+  ref: "Booking",
+  localField: "_id",
+  foreignField: "camp",
+  justOne: false,
+});
+
+module.exports = mongoose.model("Camp", CampSchema);
